ddfbc8681522fea1b823d37b8637da6ce51d3963
Vignan Nallani <vignanchowdarytilt@gmail.com>
2026-05-31 23:37:39 +0530

    fix: switch cookie browser from edge to chrome

--- backend/extractors/instagram.py
+++ backend/extractors/instagram.py
@@ -103,7 +103,7 @@ def main():
         'no_warnings': True,
         'writesubtitles': True,
         'writeautomaticsub': True,
-        'cookiesfrombrowser': ('edge',),
+        'cookiesfrombrowser': ('chrome',),
     }
 
     try:
